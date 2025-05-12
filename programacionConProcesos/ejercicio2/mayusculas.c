12b9692c731f55c3797d52703f279477dcb92148
adrus <permasadrian@gmail.com>
2025-05-12 02:04:24 +0200

    Ahora se envia el numero de bytes leidos en lugar del buffer completo

--- programacionConProcesos/ejercicio2/mayusculas.c
+++ programacionConProcesos/ejercicio2/mayusculas.c
@@ -11,12 +11,10 @@
 int main(int argc, char const *argv[]) {
     int *p1 = (int*)malloc(sizeof(int) * FD_SIZE); // HIJO -> PADRE (PADRE LEE, HIJO ESCRIBE)
     int *p2 = (int*)malloc(sizeof(int) * FD_SIZE); // PADRE -> HIJO (HIJO LEE, PADRE ESCRIBE)
+    char *buffer = (char*)malloc(sizeof(char) * SIZE_BUFFER);
     int numberBytes;
     int i;
-    char *buffer = (char*)malloc(sizeof(char) * SIZE_BUFFER);
     
-    
-   
 
     if (pipe(p1) == -1) {
         fprintf(stderr, "Error al crear el pipe 1\n");
@@ -34,6 +32,7 @@ int main(int argc, char const *argv[]) {
         fprintf(stderr, "Error al crear proceso hijo");
         return -1;
     }
+
     //Proceso hijo
     if (id == 0) {
         close(p1[0]);
@@ -48,7 +47,7 @@ int main(int argc, char const *argv[]) {
                     buffer[i] -= DIFF_MAYUS_MINUS;
                 }
             }
-            if (write(p1[1], buffer, SIZE_BUFFER) == -1) {
+            if (write(p1[1], buffer, numberBytes) == -1) {
                 fprintf(stderr, "[Hijo] Error al escribir en el pipe\n");
                 return -1;
             }
@@ -60,26 +59,31 @@ int main(int argc, char const *argv[]) {
     }else {
         close(p1[1]);
         close(p2[0]);
+
         while ((numberBytes = read(0, buffer, SIZE_BUFFER)) > 0) {
             if (numberBytes == -1) {
                 fprintf(stderr, "[PADRE] Error al leer de stdin\n");
                 return -1;
             }
             buffer[numberBytes] = '\0';
-            if (write(p2[1], buffer, SIZE_BUFFER) == -1) {
+            if (write(p2[1], buffer, numberBytes + 1) == -1) {
                 fprintf(stderr, "[PADRE] Error al escribir del pipe\n");
             }
-            if (read(p1[0], buffer, SIZE_BUFFER) == -1) {
+            if (read(p1[0], buffer, numberBytes + 1) == -1) {
                 fprintf(stderr, "[PADRE] Error al leer del pipe\n");
             }
             fprintf(stdout, "%s", buffer);
         }
+
         close(p1[0]);
         close(p2[1]);
+
         wait(NULL);
     }
+
     free(p1);
     free(p2);
     free(buffer);
+
     return 0;
 }
