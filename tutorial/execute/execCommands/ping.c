3ca32a7b1b3a5e655bdb2c24994e53390e28ae54
adrus <permasadrian@gmail.com>
2025-05-12 04:32:50 +0200

    Actualizado para rederigir la salida estandar (stdout) al fichero pingResults.txt (lo crea si no existe usando O_CREAT como parametro en la funcion open())

--- tutorial/execute/execCommands/ping.c
+++ tutorial/execute/execCommands/ping.c
@@ -4,6 +4,7 @@
 
 #include <stdio.h>
 #include <unistd.h>
+#include <fcntl.h>
 #include <stdlib.h>
 
 #include <sys/wait.h>
@@ -15,11 +16,21 @@ int main (int argc, char **argv) {
         fprintf(stderr, "Error al crear un proceso nuevo\n");
         return -1;
     }
+    //Proceso Hijo
     if (pid == 0) {
+        int file = open("pingResults.txt", O_WRONLY | O_CREAT, 0777); //O_WRONLY -> indica que solo queremos escribir | O_CREAT -> crear el fichero si no existe, 0777 -> todos los permisos para el fichero
+        if (file == -1) {
+            fprintf(stderr, "Error al abrir / crear fichero\n");
+            return -1;
+        }
+        int file2 = dup2(file, STDOUT_FILENO);
+        close(file);
         int err;
-        err = execlp("ping", "ping", "-c", "3", "google.com", NULL);
+        err = execlp("pinasfsafg", "ping", "-c", "3", "google.com", NULL);
         perror("execvp");
         exit(-1);
+
+    // Proceso Padre
     } else {
         int wstatus;
         wait(&wstatus);
