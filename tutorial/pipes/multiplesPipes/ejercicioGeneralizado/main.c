332e3256e5c8c293715c16df86dcd99b5876d5ee
adrus <permasadrian@gmail.com>
2025-05-15 03:05:11 +0200

    Ejercicio como el ../main.c pero generalizado para crear n procesos (sin hacer)

---
+++ tutorial/pipes/multiplesPipes/ejercicioGeneralizado/main.c
@@ -0,0 +1,15 @@
+/*
+Generalizar el ejercicio ../main.c en el que se generan 'n' procesos y se necesitaran m pipes
+*/
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <sys/wait.h>
+
+
+int main(int argc, char const *argv[])
+{
+    
+
+    return 0;
+}
